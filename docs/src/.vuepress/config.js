70995e367ce292f7943e2e8c83f977d621ae0bc4
martin mutuma <martin.mutuma@systechafrica.com>
2023-11-21 12:13:34 +0300

    logo change

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,11 +1,11 @@
 const { description } = require('../../package');
 
 module.exports = {
-  logo: "/img/logo/fundmasterlogo",
+  logo: "/img/fundmasterlogo",
   title: 'FundMaster Xe',
   description: description,
   themeConfig: {
-    logo: "/img/logo/fundmasterlogo",
+    logo: "/img/fundmasterlogo",
     searchPlaceholder: 'Search...',
     search:true,
     lastUpdated: 'Last Updated',
