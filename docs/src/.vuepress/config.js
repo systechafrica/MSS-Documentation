725f4bda9fef928fcd572c7dee3e13da6ea09d35
martin mutuma <martin.mutuma@systechafrica.com>
2023-11-23 10:29:06 +0300

    edits

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
       {
         text: 'Deployment Guide',
-        link: '/deployment/'    
+        link: '/videotutorials/'    
       },
       {
         text: ' Video Tutorials',
